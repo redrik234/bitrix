7125341fe6ae296b4470ee6a65b2a3abca4821bc
redrik234 <illarionow.artem@yandex.ru>
2022-12-16 23:48:44 +0300

    Добавлен компонент "Каталог товаров"

---
+++ local/components/test_task/simplecomp.catalog/.description.php
@@ -0,0 +1,10 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); 
+$arComponentDescription = array(
+    "NAME" => GetMessage("SС_NAME"),
+    "DESCRIPTION" => GetMessage("SС_DESCRIPTION"),
+    "PATH" => array(
+        "ID" => "local",
+        "NAME" => GetMessage("SC_PATH_NAME")
+    ),
+);
+?>
