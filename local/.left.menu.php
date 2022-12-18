4c7927bc0f54eed52ab1215ea54c5e5302d3635e
redrik234 <illarionow.artem@yandex.ru>
2022-12-18 19:54:23 +0300

    Добавлен в local левое меню

---
+++ local/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Форма обратной связи",
+		"/local/feedback/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Простой компонент",
+		"/local/simplecomp/",
+		Array(),
+		Array(),
+		""
+	)
+);
+?>
